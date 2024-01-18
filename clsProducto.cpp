ceea81197acce0f3fba5ff84b260fcd20df5b1ee
DavidMontoyaHolgado <dgmontoyah@unjbg.edu.pe>
2024-01-18 18:11:16 -0500

    Se comento los metodos de retorno de valores para probar el uso de .txt

--- clsProducto.cpp
+++ clsProducto.cpp
@@ -4,32 +4,38 @@
 using namespace std;
 
 class clsProducto{
-    public:
-        string nombre;
-        float precio;
-        string descripcion;
-        string marca;
-        string modelo;
+    private:
+        // string nombreProducto;
+        // float precio;
+        // string descripcion;
+        // string marca;
+        // string modelo;
         string categoria;
         string categoria_tipo;
         string colores[10];
         int cantColor;
         clsDatosAdicionales Stock;
     public:
-        clsProducto(string, float, string, string, string, string,string, string*,int);
-        void llenarStockP();
-        void llenarStockP(int, string *);
+        clsProducto(string,string, string*,int);
+        // string getNombre() const {return nombreProducto;};
+        // float getPrecio() const {return precio;};
+        // string getDescripcion () const {return descripcion;};
+        // string getMarca () const {return marca;};        
+        // string getModelo () const {return modelo;};
+        // string getCategoria () const {return ;};
+        // string getColores    () const {return ;};
         string setNombre();
         float setPrecio();
-        void mostrarDatos();
+        void llenarStockP();
+        void llenarStockP(int, string *);
 };
 
-clsProducto::clsProducto(string nombreP, float precioP, string descripcionP, string marcaP,string modeloP, string _categoria, string categoria_tipo,string* coloresP, int cantColorP){
-    nombre = nombreP;
-    precio = precioP;
-    descripcion = descripcionP;
-    marca = marcaP;
-    modelo = modeloP;
+clsProducto::clsProducto(string _categoria, string categoria_tipo,string* coloresP, int cantColorP){
+    // nombreProducto = nombreP;
+    // precio = precioP;
+    // descripcion = descripcionP;
+    // marca = marcaP;
+    // modelo = modeloP;
     categoria = _categoria;
     categoria_tipo = categoria_tipo;
     cantColor = cantColorP;
@@ -41,26 +47,3 @@ clsProducto::clsProducto(string nombreP, float precioP, string descripcionP, str
 void clsProducto::llenarStockP(){Stock.llenarStock(cantColor, colores);}
 
 void clsProducto::llenarStockP(int cantTalla, string* talla){Stock.llenarStock(cantColor, cantTalla,colores, talla);}
-
-string clsProducto::setNombre(){return nombre;}
-
-float clsProducto::setPrecio(){return precio;}
-
-void clsProducto::mostrarDatos(){
-    cout<<"Nombre "<<nombre<<endl;
-    cout<<"Marca: "<<marca<<endl;
-    cout<<"Precio: "<<precio<<endl;
-    cout<<"Descripcion: "<<descripcion<<endl;
-}
-
-int main(){
-    string colores[] = {"Rojo", "Morado", "Azul", "Amarillo"};
-    clsProducto Celular("aifon 20", 20, "Es de buena calidad", "iafon", "12", "Tecnologia", "Celulares", colores,4); 
-    Celular.mostrarDatos();
-    Celular.llenarStockP();
-    int* Datos = Celular.Stock.getArreglo();
-    for(int i = 0; i < 4;i++){
-        cout<<Datos[i]<<' ';
-    }
-    return 0;
-}
