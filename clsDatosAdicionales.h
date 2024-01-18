02b507716367c9dda8fc5bd64a7bdcc6c2c0497c
DavidMontoyaHolgado <dgmontoyah@unjbg.edu.pe>
2024-01-18 01:31:01 -0500

    Se sobrecargo el metodo llenarStock y se agrego un metodo para retornar arreglo

--- clsDatosAdicionales.h
+++ clsDatosAdicionales.h
@@ -1,31 +1,18 @@
 #include <iostream>
 #include <string>
 using namespace std;
-string colores []= {"Rojo","verde","azul","morado","anaranjado", };
 class clsDatosAdicionales{
     private:
-        string ejeX;
-        string ejeY;
-        int fila;
-        int columna;
         int matriz[10][5];
     public:
-        clsDatosAdicionales(int, int);
-        void llenarStock(string *, string *);
+        clsDatosAdicionales(){}
+        void llenarStock(int, int, string *, string *);
         int (*getMatriz())[5];
-        void mostrarDatos();
 };
 
-clsDatosAdicionales::clsDatosAdicionales(int _cantX, int _cantY){
-    ejeX = "Talla";
-    ejeY = "Color";
-    fila = _cantX;
-    columna = _cantY;
-}
-
-void clsDatosAdicionales::llenarStock(string *_color, string *_talla){
-    for(int i = 0; i < fila; i++){
-        for(int j = 0; j < columna;j++){
+void clsDatosAdicionales::llenarStock(int _cantX, int _cantY, string *_color, string *_talla){
+    for(int i = 0; i < _cantX; i++){
+        for(int j = 0; j < _cantY;j++){
             cout<<"Ingresa "<<_color[i]<<" talla "<<_talla[j]<<": ";
             cin>>matriz[i][j];
         }
