eeba8ecfb2ed166c9d59302f133334a8a2685a3e
Alex Huaracha Bellido <ayhuarachab@unjbg.edu.pe>
2024-01-20 01:13:55 -0500

    mejora del programa Carrito

--- clsCarrito.cpp
+++ clsCarrito.cpp
@@ -20,6 +20,7 @@ class Carrito{
 	 void anadirCarrito(string,double,int);
 	 void quitarCarrito(int);
 	 void mostrarCarrito();
+	 double calculaTotal();
 	 int getCompra();
 	 int getCantidad();		
 };
@@ -33,31 +34,31 @@ Carrito::Carrito(){
 
 void Carrito::anadirCarrito(string nombre,double precio,int Npro){
 	
-	if(cantidad<Npro){
+//	if(cantidad<Npro){
 		compras[cantidad].nombre =nombre;
 		compras[cantidad].precio = precio;
 		cantidad++;
 		cout<<endl;
 		cout<<" Producto agregado al carrito: "<<nombre<<endl;
-	}else{
-		cout<<"El carrito esta lleno no se pueden agregar mas productos.  "<<endl;
-	}
+	//}else{
+	//	cout<<"El carrito esta lleno no se pueden agregar mas productos.  "<<endl;
+	//}
 }
 
 void Carrito::quitarCarrito(int indice){
-	if(indice>=0 and indice<cantidad){
+//	if(indice>=0 and indice<cantidad){
 		
 	    cout << "Producto eliminado del carrito: " << compras[indice-1].nombre << endl;
 	     
-	     for(int i=indice-1;i<cantidad-1;i++){
+	     for(int i=indice;i<cantidad;i++){
 	     	
-	     	compras[i]=compras[i+1];
+	     	compras[i-1]=compras[i];
 		 }
 		 cantidad--;
-	} else{
+	//} else{
 		
-		cout<<"Indeice de producto no valido. "<<endl;
-	}
+	//	cout<<"Indeice de producto no valido. "<<endl;
+	//}
 	
 }
  
@@ -80,6 +81,14 @@ void Carrito::quitarCarrito(int indice){
         cout << "Total de productos: " << cantidad << endl;
  }
 
+double Carrito::calculaTotal(){
+	
+	double total = 0.0;
+        for (int i = 0; i < cantidad; i++) {
+            total += compras[i].precio;
+        }
+        return total;
+}
 
 
 
@@ -87,13 +96,76 @@ int main(){
 	
 	string producto;
 	int precio;
-	
+	int quitar;
+	int opc;
 	Carrito carritov;
-	
 	
 	int cant;
 	
-	cout<<"ingrese la cantidad de productos : "; cin>>cant;
+	
+	
+	do{
+		
+		cout<<"1. Ingresar productos. "<<endl;
+		cout<<"2. Mostrar lista de productos."<<endl;
+		cout<<"3. Eliminar productos"<<endl;
+		cout<<"4.salir de CARRITO."<<endl;
+		cout<<endl;
+		cout<<"ingrese la opcion: "; cin>>opc;
+		cout<<endl;
+		
+		switch(opc){
+			
+			case 1:
+				  //cout<<endl;
+				  //cout<<"ingrese la cantidad de productos : "; cin>>cant;
+			         //for(int i=0;i<cant;i++){
+			         	 cout<<endl;
+			         	 cout<<+1<<".Producto: ";
+			         	 cin>>producto;
+		                 cout<<"Precio : ";
+	                 	cin>>precio;
+	                  	carritov.anadirCarrito(producto,precio,cant);
+					 //}
+			          cout<<endl;
+	
+                  	 carritov.mostrarCarrito();
+	                  cout << "Total de la compra: $" << carritov.calculaTotal() << endl;
+	                  cout<<endl;
+			      break;
+			
+			case 2:
+			       carritov.mostrarCarrito();
+				   cout << "Total de la compra: $" << carritov.calculaTotal() << endl;
+				   cout<<endl;    
+	              break;
+			
+			case 3:	  
+			         cout<<"Ingrese numero de producto a eliminar: "; cin>>quitar;
+	
+                    carritov.quitarCarrito(quitar);
+                    cout<<endl;
+			       break;
+			 case 4:
+			 	 break;
+			 		
+		}
+		
+		
+		
+		
+	}while(opc!=4);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+/*	cout<<"ingrese la cantidad de productos : "; cin>>cant;
 	
 	for(int i=0;i<cant;i++)
 	{
@@ -107,9 +179,18 @@ int main(){
 	cout<<endl;
 	
 	carritov.mostrarCarrito();
+	cout << "Total de la compra: $" << carritov.calculaTotal() << endl;
+
 	cout<<endl;
-	carritov.quitarCarrito(2);
+	
+	cout<<"Ingrese numero de producto a eliminar: "; cin>>quitar;
+	
+	carritov.quitarCarrito(quitar);
 	cout<<endl;
+	
+	
 	carritov.mostrarCarrito();
+	cout << "Total de la compra: $" << carritov.calculaTotal() << endl; */
+
 	return 0;
 }
