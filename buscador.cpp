ad55977fd973aca94ddbeb6b99c10a9f52fc1f20
Alex-Huaracha-Bellido <ayhuarachab@unjbg.edu.pe>
2024-01-23 19:15:43 -0500

    subiendo los archivos .h y .cpp

---
+++ buscador.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<fstream>
+#include "buscador.h"
+
+using namespace std;
+
+Buscador::Buscador(string _producto){
+	
+	producto=_producto;
+}
+
+// *A =tienda
+void Buscador::clsBuscador(string *A,int n, string tienda){
+	
+	int pos=0;
+	   int i=0;
+	
+	while(i<n and A[i]!=tienda){
+		
+		i=i+1;
+	}
+	
+	if(i>n or A[i]==tienda){
+		pos=i+1;
+	}else{
+		pos=-i-1;
+	}
+
+
+	if(pos>0){
+    	
+    	cout<<"La tienda  "<<tienda<<" si existe "<<pos<<endl;
+	}else{
+		
+		
+		cout<<"No se encontro dicha tienda"<<endl;
+	}
+	
+}
+
+
+
+int main(){
+	
+	string A[6]= {"D'moda", "sagaFalavela","Electro","@live","Efe" , "El_gallo"}; //correos
+	const int n = sizeof(A) / sizeof(A[0]);
+	
+	string tnd;
+	
+	Buscador tienda("celular");
+	
+	cout<<"ingrese la tienda que desea: "; cin>>tnd; 
+	
+	tienda.clsBuscador(A,n,tnd);
+	
+	return 0;
+}
